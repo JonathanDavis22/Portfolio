40c90e37db3dad8be580f40be3867a3f47c6f091
jon91@utexas.edu <jon91@utexas.edu>
2019-07-11 12:44:17 -0700

    Portfolio with P5.js play. Not mobile-friendly yet

---
+++ js/sketch.js
@@ -0,0 +1,32 @@
+// Daniel Shiffman
+// http://codingtra.in
+// http://patreon.com/codingtrain
+// Code for: https://youtu.be/KkyIDI6rQJI
+
+// Purple Rain
+// (138, 43, 226)
+// (230, 230, 250) // background
+
+var drops = [];
+var canvas;
+
+// function windowResized() {
+//   resizeCanvas(windowWidth, windowHeight);
+// }
+
+function setup() {
+  canvas = createCanvas(1900, 5400);
+  canvas.position(0, 0);
+  canvas.style('z-index', '-1');
+  for (var i = 0; i < 500; i++) {
+    drops[i] = new Drop();
+  }
+}
+
+function draw() {
+  background(230, 230, 250);
+  for (var i = 0; i < drops.length; i++) {
+    drops[i].fall();
+    drops[i].show();
+  }
+}
